9de4f39aa825a36920db5c968c4af73f6e30e29f
desyna <90520848+desyna@users.noreply.github.com>
2025-11-17 20:00:34 +0700

    Add files via upload

---
+++ frontend/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '5000',
+        pathname: '/static/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
